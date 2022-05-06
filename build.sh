f08dfa016c0de9fcaf8c77d527aca680159458a3
chautruonglong <truonglongchau@gmail.com>
2022-05-06 08:55:33 +0700

    Genrate SQL script

--- build.sh
+++ build.sh
@@ -2,21 +2,21 @@
 
 set -x
 
-export MSSQL_DB=AspNetTutorial
-export MSSQL_USER=sa
-export MSSQL_PASSWORD=Ctlbi@0775516337
+export MSSQL_DB="AspNetTutorial"
+export MSSQL_USER="sa"
+export MSSQL_PASSWORD="Ctlbi@0775516337"
 
-export CONNECTION_STRING=Data Source=sql-server-tutorial,1433;Database=${MSSQL_DB};Integrated Security=false;User ID=${MSSQL_USER};Password=${MSSQL_PASSWORD};
+export CONNECTION_STRING="Data Source=sql-server-tutorial,1433;Database=${MSSQL_DB};Integrated Security=false;User ID=${MSSQL_USER};Password=${MSSQL_PASSWORD};"
 
-export DOCKER_EXTERNAL_RESOURCES=/asp-net-tutorial/resources
-export EXTERNAL_RESOURCES=C:/Users/chautruonglong/Desktop/resources
+export DOCKER_EXTERNAL_RESOURCES="/asp-net-tutorial/resources"
+export EXTERNAL_RESOURCES="/root/app/resources"
 
-export SECRET_KEY=JRi824hxq7m1vjVlpE88fWxLfOfF9nszm5CiE9hWFffvSRQTZ7
+export SECRET_KEY="JRi824hxq7m1vjVlpE88fWxLfOfF9nszm5CiE9hWFffvSRQTZ7"
 
-export API_BASE_URL=http://api.mvg-sky.com/api
+export API_BASE_URL="http://api.mvg-sky.com/api"
 
-docker-compose build
-docker-compose up -d
+docker compose build
+docker compose up -d
 
 # shellcheck disable=SC2046
 docker rmi $(docker images -f "dangling=true" -aq)
